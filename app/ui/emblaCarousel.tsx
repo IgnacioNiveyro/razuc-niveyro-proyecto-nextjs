2107d9b1d3be39e2fd3fe5267d985b95152afa57
IgnacioNiveyro <nacho_niveyro@hotmail.com>
2024-05-29 00:36:38 -0300

    Embla carousel

---
+++ app/ui/emblaCarousel.tsx
@@ -0,0 +1,78 @@
+'use client'
+import React, { useCallback, useEffect, useState } from 'react'
+import { EmblaOptionsType } from 'embla-carousel'
+import useEmblaCarousel from 'embla-carousel-react'
+import AutoScroll from 'embla-carousel-auto-scroll'
+import {
+  NextButton,
+  PrevButton,
+  usePrevNextButtons
+} from './EmblaCarouselArrowButtons'
+import Image from 'next/image';
+
+type PropType = {
+  slides: number[]
+  options?: EmblaOptionsType
+}
+
+const EmblaCarousel: React.FC<PropType> = (props) => {
+  const { slides, options } = props
+  const [emblaRef, emblaApi] = useEmblaCarousel(options, [
+    AutoScroll({ playOnInit: true })
+  ])
+  const [isPlaying, setIsPlaying] = useState(false)
+
+  const {
+    prevBtnDisabled,
+    nextBtnDisabled,
+    onPrevButtonClick,
+    onNextButtonClick
+  } = usePrevNextButtons(emblaApi)
+
+  const onButtonAutoplayClick = useCallback(
+    (callback: () => void) => {
+      const autoScroll = emblaApi?.plugins()?.autoScroll
+      if (!autoScroll) return
+
+      const resetOrStop = true;
+
+      callback()
+    },
+    [emblaApi]
+  )
+
+  const toggleAutoplay = useCallback(() => {
+    const autoScroll = emblaApi?.plugins()?.autoScroll
+    if (!autoScroll) return
+
+  }, [emblaApi])
+
+  useEffect(() => {
+    const autoScroll = emblaApi?.plugins()?.autoScroll
+    if (!autoScroll) return
+
+
+  }, [emblaApi])
+
+  return (
+    <div className="embla">
+      <div className="embla__viewport" ref={emblaRef}>
+        <div className="embla__container">
+          {slides.map((index) => (
+            <div className="embla__slide" key={index}>
+              <div className="embla__slide__number">
+                <div className="bg-red-500 border border-black">
+                  <p>{index + 1}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+
+    </div>
+  )
+}
+
+export default EmblaCarousel
