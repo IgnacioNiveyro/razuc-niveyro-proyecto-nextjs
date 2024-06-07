b0ec36ac510125ef5558af05ad99c2fd2826f9e5
IgnacioNiveyro <nacho_niveyro@hotmail.com>
2024-06-07 14:39:02 -0300

    trying to fix edit book

--- app/home/id/edit/page.tsx
+++ app/home/id/edit/page.tsx
@@ -1,27 +1,3 @@
-import Form from '@/app/ui/edit-form';
-import Breadcrumbs from '@/app/ui/breadcrumbs';
-import { fetchBookById, fetchAllBooks } from '@/app/lib/data';
-import { Book,  BookForm } from '@/app/lib/definitions'
- 
-export default async function Page({ params }: { params: { id: string } }) {
-    const id = params.id;
-    const [form, books]: [BookForm, Book[]] = await Promise.all([
-        fetchBookById(id),
-        fetchAllBooks(),
-      ]);
-  return (
-    <main>
-      <Breadcrumbs
-        breadcrumbs={[
-          { label: 'Books', href: '/home' },
-          {
-            label: 'Edit Book',
-            href: `/home/${id}/pick-book`,
-          
-          },
-        ]}
-      />
-      <Form books={books} form={form} />
-    </main>
-  );
+export default function Page() {
+  return (<p>Favoritos Page</p>);
 }
