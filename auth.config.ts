87f7c5966953f60d4c7bf85e78fcbe206779f752
IgnacioNiveyro <nacho_niveyro@hotmail.com>
2024-06-19 16:32:41 -0300

    login now redirects successfully

--- auth.config.ts
+++ auth.config.ts
@@ -1,10 +1,22 @@
 import type { NextAuthConfig } from 'next-auth';
- 
+import { redirect } from 'next/navigation';
+import { NextResponse } from 'next/server';
+
 export const authConfig = {
   pages: {
     signIn: '/login',
   },
   callbacks: {
+    async signIn({ user, account, profile }) {
+      return true;
+    },
+    async redirect({ url, baseUrl }) {
+      if (url.startsWith(baseUrl)) return url;
+      return baseUrl + '/home';
+    },
+    async session({ session, token }) {
+      return session;
+    },
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
       const isOnHome = nextUrl.pathname.startsWith('/home');
@@ -15,6 +27,7 @@ export const authConfig = {
         nextUrl.pathname.match(/^\/home\/[^\/]+\/edit$/) ||
         nextUrl.pathname.startsWith('/home/pick-book');
       
+      const loginRoutes = nextUrl.pathname.startsWith('/login')
 
       if(adminRoutes){
         
@@ -24,6 +37,10 @@ export const authConfig = {
           return false; // Va al login
 
       }else{
+        if (loginRoutes && isLoggedIn) {
+          const absoluteUrl = new URL('/home', nextUrl.origin);
+          return NextResponse.redirect(absoluteUrl.toString());
+        }
         if(isOnHome)
           return true;
       }
